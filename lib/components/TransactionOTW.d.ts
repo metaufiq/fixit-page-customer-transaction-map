d3df0abf697b4ed1d8aba6618e100b5a2c67d12a
Muh.Taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-01-15 12:25:07 +0800

    update lib

--- lib/components/TransactionOTW.d.ts
+++ lib/components/TransactionOTW.d.ts
@@ -1,11 +1,11 @@
 /// <reference types="react" />
 import { NavigationProp, RouteProp } from '@react-navigation/native';
 import Transaction from '../config/types/domain/transaction';
-interface mainProps {
+interface Props {
     navigation: NavigationProp<any, any>;
     route: RouteProp<Record<any, {
         transaction: Transaction;
     }>, any>;
 }
-declare const TransactionOTW: (props: mainProps) => JSX.Element;
+declare const TransactionOTW: (props: Props) => JSX.Element;
 export default TransactionOTW;
